1e4adcaa16b11c73bc5d4e1c4c3e544dacd8fecf
ddnold <s532534@nwmissouri.edu>
2022-04-14 09:44:38 -0500

    Did part 1 and 2 of assignment 12

--- controllers/farmhands.js
+++ controllers/farmhands.js
@@ -25,9 +25,16 @@ exports.farmhands_view_all_Page = async function(req, res) {
     }   
 }; 
  
-// for a specific farmhands. 
-exports.farmhands_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Farmhands detail: ' + req.params.id); 
+// for a specific Farmhand. 
+exports.farmhands_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Farmhand.findById(req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle Costume create on POST. 
@@ -40,7 +47,7 @@ exports.farmhands_create_post = async function(req, res) {
     // {"costume_type":"goat", "cost":12, "size":"large"} 
     document.name = req.body.name; 
     document.age = req.body.age; 
-    document.height = req.body.height; 
+    document.height = req.body; 
     try{ 
         let result = await document.save(); 
         res.send(result); 
@@ -56,7 +63,21 @@ exports.farmhands_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Farmhands delete DELETE ' + req.params.id); 
 }; 
  
-// Handle farmhands update form on PUT. 
-exports.farmhands_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Farmhands update PUT' + req.params.id); 
+// Handle Costume update form on PUT. 
+exports.farmhands_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Farmhand.findById(req.params.id) 
+        // Do updates of properties 
+        if(req.body.name)  
+               toUpdate.name = req.body.name; 
+        if(req.body.cost) toUpdate.age = req.body.age; 
+        if(req.body.size) toUpdate.height = req.body.height; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 

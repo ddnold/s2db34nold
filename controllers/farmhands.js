bf4fa1539ddc88edd361d1c42506b6ac12d9ba74
ddnold <s532534@nwmissouri.edu>
2022-04-20 17:01:27 -0500

    Added part 7

--- controllers/farmhands.js
+++ controllers/farmhands.js
@@ -99,6 +99,20 @@ exports.farmhands_delete = async function(req, res) {
         res.send(`{"error": Error deleting ${err}}`); 
     } 
 }; 
+
+// Handle a delete one view with id from query 
+exports.farmhands_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Farmhand.findById(req.query.id) 
+        res.render('farmhandsdelete', { title: 'Farmhand Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
  
 // Handle Costume update form on PUT. 
 exports.farmhands_update_put = async function(req, res) { 
--- routes/farmhands.js
+++ routes/farmhands.js
@@ -14,4 +14,7 @@ router.get('/create', farmhands_controllers.farmhands_create_Page);
 /* GET create update page */ 
 router.get('/update', farmhands_controllers.farmhands_update_Page);
 
+/* GET delete costume page */ 
+router.get('/delete', farmhands_controllers.farmhands_delete_Page);
+
 module.exports = router; 

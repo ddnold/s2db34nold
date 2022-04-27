c8e9cb5ce57a7853054dffbcc238bc0065e6d8cb
ddnold <s532534@nwmissouri.edu>
2022-04-26 23:40:51 -0500

    Added part 3, assignment 13

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ passport.use(new LocalStrategy(
       } 
       return done(null, user); 
     }); 
-  }))
+  } 
+))
 var Farmhand = require("./models/farmhands");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -88,6 +89,12 @@ app.use('/farmhands', farmhandsRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+var Account = require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
+ 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

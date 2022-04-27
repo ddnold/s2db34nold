d7d6fbf2ce4ab366f6afc4802cd1aa8f3286a2aa
ddnold <s532534@nwmissouri.edu>
2022-04-26 23:48:22 -0500

    Added part 4, assignment 13

--- routes/farmhands.js
+++ routes/farmhands.js
@@ -1,6 +1,14 @@
 var express = require('express'); 
 const farmhands_controllers= require('../controllers/farmhands'); 
 var router = express.Router(); 
+
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET farmhands */ 
 router.get('/', farmhands_controllers.farmhands_view_all_Page); 
@@ -12,7 +20,7 @@ router.get('/detail', farmhands_controllers.farmhands_view_one_Page);
 router.get('/create', farmhands_controllers.farmhands_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', farmhands_controllers.farmhands_update_Page);
+router.get('/update', secured, farmhands_controllers.farmhands_update_Page);
 
 /* GET delete costume page */ 
 router.get('/delete', farmhands_controllers.farmhands_delete_Page);
